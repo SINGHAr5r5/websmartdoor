a7b5eee50e8191f4d132dec50cb700cea790a8e5
SINGHAr5r5 <sing_love108@hotmail.com>
2020-05-18 05:45:08 +0700

    test

--- web/member/index.php
+++ web/member/index.php
@@ -6,7 +6,7 @@ require_once('../connect.php');
 <!DOCTYPE HTML>
 <html>
 	<head>
-	<meta charset="utf-8">
+	<meta charset="utf-8" >
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Jackson Template</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
